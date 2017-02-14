44ff2e9f8eb7349b0acd9a0396757d1acde5f61d
Pakovia <pavol.tomasovic@gmail.com>
2017-02-14 13:34:41 +0100

    Create sk.js
    Slovak translation

---
+++ src/locale/lang/sk.js
@@ -0,0 +1,87 @@
+export default {
+  el: {
+    datepicker: {
+      now: 'Teraz',
+      today: 'Dnes',
+      cancel: 'Zrušiť',
+      clear: 'Zmazať',
+      confirm: 'OK',
+      selectDate: 'Vybrať dátum',
+      selectTime: 'Vybrať čas',
+      startDate: 'Dátum začatia',
+      startTime: 'Čas začatia',
+      endDate: 'Dátum ukončenia',
+      endTime: 'Čas ukončenia',
+      day: 'Deň',
+      week: 'Týždeň',
+      month: 'Mesiac',
+      year: 'Rok',
+      month1: 'Január',
+      month2: 'Február',
+      month3: 'Marec',
+      month4: 'Apríl',
+      month5: 'Máj',
+      month6: 'Jún',
+      month7: 'Júl',
+      month8: 'August',
+      month9: 'September',
+      month10: 'Október',
+      month11: 'November',
+      month12: 'December',
+      weeks: {
+        sun: 'Ne',
+        mon: 'Po',
+        tue: 'Ut',
+        wed: 'St',
+        thu: 'Št',
+        fri: 'Pi',
+        sat: 'So'
+      },
+      months: {
+        jan: 'Jan',
+        feb: 'Feb',
+        mar: 'Mar',
+        apr: 'Apr',
+        may: 'Maj',
+        jun: 'Jun',
+        jul: 'Jul',
+        aug: 'Aug',
+        sep: 'Sep',
+        oct: 'Okt',
+        nov: 'Nov',
+        dec: 'Dec'
+      }
+    },
+    select: {
+      loading: 'Nahrávam',
+      noMatch: 'Žiadna zhoda',
+      noData: 'Bez dát',
+      placeholder: 'Vybrať dáta'
+    },
+    pagination: {
+      goto: 'Ísť na',
+      pagesize: 'na stranu',
+      total: 'Celkovo {total}',
+      pageClassifier: ''
+    },
+    messagebox: {
+      confirm: 'OK',
+      cancel: 'Zrušiť',
+      error: 'Chyba'
+    },
+    upload: {
+      delete: 'Vymazať',
+      preview: 'Náhľad',
+      continue: 'Pokračovať'
+    },
+    table: {
+      emptyText: 'Prázdne políčko',
+      confirmFilter: 'Potvrď filter',
+      resetFilter: 'Zresetovať filter',
+      clearFilter: 'Zrušiť filter'
+    },
+    tree: {
+      emptyText: 'Prázdne políčko'
+    }
+  }
+};
